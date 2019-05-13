114e80485b3f8db47c6d454110bd80ba1dd26829
tomas.forne-cappeau@epitech.eu <tomas.forne-cappeau@epitech.eu>
2019-05-13 16:04:01 +0200

    [ADD] test_my_strchrs.c Adding more unit tests on my_strchrs.c

--- tests/string/test_my_strchrs.c
+++ tests/string/test_my_strchrs.c
@@ -18,3 +18,44 @@ Test(my_strchrs, valid)
     actual = my_strchrs(param1, param2);
     cr_assert_str_eq(actual, expected);
 }
+
+Test(my_strchrs, null_delimiter)
+{
+    char *param1 = "Hello, World!";
+    char *param2 = NULL;
+    char *actual = NULL;
+
+    actual = my_strchrs(param1, param2);
+    cr_assert_null(actual);
+}
+
+Test(my_strchrs, null_src)
+{
+    char *param1 = NULL;
+    char *param2 = "e!";
+    char *actual = NULL;
+
+    actual = my_strchrs(param1, param2);
+    cr_assert_null(actual);
+}
+
+Test(my_strchrs, empty_occurences)
+{
+    char *param1 = "Hello, Wolrd!";
+    char *param2 = "";
+    char *actual = NULL;
+
+    actual = my_strchrs(param1, param2);
+    cr_assert_null(actual);
+}
+
+Test(my_strchrs, first_occurence_not_in_str)
+{
+    char *param1 = "abcd";
+    char *param2 = "ec";
+    char *actual = NULL;
+    char *expected = "cd";
+
+    actual = my_strchrs(param1, param2);
+    cr_assert_str_eq(actual, expected);
+}
