b2818a013e9173f4e2f74c60ce4d12b98da09072
tomas.forne-cappeau@epitech.eu <tomas.forne-cappeau@epitech.eu>
2019-05-13 15:47:16 +0200

    [ADD] test_my_strchr.c Adding more tests for my_strchr.c

--- tests/string/test_my_strchr.c
+++ tests/string/test_my_strchr.c
@@ -18,3 +18,33 @@ Test(my_strchr, valid)
     actual = my_strchr(param1, param2);
     cr_assert_str_eq(actual, expected);
 }
+
+Test(my_strchr, null_param1)
+{
+    char *param1 = NULL;
+    char param2 = 'o';
+    char *ret = NULL;
+
+    ret = my_strchr(param1, param2);
+    cr_assert_null(ret);
+}
+
+Test(my_strchr, occurence_is_end_of_string)
+{
+    char *param1 = "Hello, World!";
+    char param2 = 0;
+    char *actual = NULL;
+
+    actual = my_strchr(param1, param2);
+    cr_assert_str_empty(actual);
+}
+
+Test(my_strchr, occurence_is_not_in_str)
+{
+    char *param1 = "abcd";
+    char param2 = 'e';
+    char *actual = NULL;
+
+    actual = my_strchr(param1, param2);
+    cr_assert_null(actual);
+}
